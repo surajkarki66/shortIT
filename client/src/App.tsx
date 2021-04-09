12ca34b0267230b0efcaffbdf7e373a8ef757356
surajkarki66 <suraj.karki500@protonmail.com>
2021-04-09 20:25:51 +0545

    Get started with client side

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,11 @@
 import React from "react";
-import { Container } from "react-bootstrap";
 
 import "./App.css";
-import NavBar from "./components/Navigation/NavBar";
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      <NavBar />
-      <Container>
-        <h1>Hello</h1>
-      </Container>
+      <h1>ShortIT</h1>
     </div>
   );
 };
