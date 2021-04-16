c1291ae6777a7c381f1631f5f2530c8c4586ee7e
surajkarki66 <suraj.karki500@protonmail.com>
2021-04-17 00:36:51 +0545

    Finished Register page

--- client/src/components/Forms/RegisterForm.tsx
+++ client/src/components/Forms/RegisterForm.tsx
@@ -44,158 +44,162 @@ const RegisterForm: React.FC<Props> = (props) => {
   };
 
   return (
-    <React.Fragment>
+    <Form
+      style={{
+        minWidth: "405px",
+        fontWeight: "bold",
+      }}
+      {...formItemLayout}
+      name="register"
+      initialValues={{ remember: true }}
+      onFinish={onFinish}
+    >
       {registerError !== "" && (
         <h4 style={{ color: "red" }}>{registerError}</h4>
       )}
-
-      <Form
-        {...formItemLayout}
-        name="register"
-        initialValues={{ remember: true }}
-        onFinish={onFinish}
+      <Form.Item
+        label="First-Name"
+        name="firstName"
+        rules={[
+          { required: true, message: "Please input your First Name" },
+          { min: 2, message: "First Name should be more than 1 character" },
+          {
+            max: 32,
+            message: "First Name should be less than 32 character",
+          },
+        ]}
       >
-        <Form.Item
-          label="First-Name"
-          name="firstName"
-          rules={[
-            { required: true, message: "Please input your First Name" },
-            { min: 2, message: "First Name should be more than 1 character" },
-            {
-              max: 32,
-              message: "First Name should be less than 32 character",
-            },
-          ]}
-        >
-          <Input
-            onChange={(event) =>
-              setUserInputData({
-                ...userInputData,
-                firstName: event.target.value,
-              })
-            }
-          />
-        </Form.Item>
-        <Form.Item
-          label="Last-Name"
-          name="lastName"
-          rules={[
-            { required: true, message: "Please input your Last Name" },
-            { min: 2, message: "Last Name should be more than 1 character" },
-            {
-              max: 32,
-              message: "Last Name should be less than 32 character",
-            },
-          ]}
-        >
-          <Input
-            onChange={(event) =>
-              setUserInputData({
-                ...userInputData,
-                lastName: event.target.value,
-              })
-            }
-          />
-        </Form.Item>
-        <Form.Item
-          name="email"
-          label="E-mail"
-          rules={[
-            {
-              type: "email",
-              message: "The input is not valid E-mail!",
-            },
-            {
-              required: true,
-              message: "Please input your E-mail!",
-            },
-          ]}
-        >
-          <Input
-            onChange={(event) =>
-              setUserInputData({
-                ...userInputData,
-                email: event.target.value,
-              })
-            }
-          />
-        </Form.Item>
-        <Form.Item
-          name="password"
-          label="Password"
-          rules={[
-            {
-              required: true,
-              message: "Please input your password!",
-            },
-            { min: 6, message: "Password should be more than 5 character" },
-            {
-              pattern: /[A-Z]/,
-              message: "Password must contain an uppercase",
-            },
-            { pattern: /[0-9]/, message: "Password must contain a number" },
-          ]}
-          hasFeedback
-        >
-          <Input.Password
-            onChange={(event) =>
-              setUserInputData({
-                ...userInputData,
-                password: event.target.value,
-              })
-            }
-          />
-        </Form.Item>
+        <Input
+          onChange={(event) =>
+            setUserInputData({
+              ...userInputData,
+              firstName: event.target.value,
+            })
+          }
+        />
+      </Form.Item>
+      <Form.Item
+        label="Last-Name"
+        name="lastName"
+        rules={[
+          { required: true, message: "Please input your Last Name" },
+          { min: 2, message: "Last Name should be more than 1 character" },
+          {
+            max: 32,
+            message: "Last Name should be less than 32 character",
+          },
+        ]}
+      >
+        <Input
+          onChange={(event) =>
+            setUserInputData({
+              ...userInputData,
+              lastName: event.target.value,
+            })
+          }
+        />
+      </Form.Item>
+      <Form.Item
+        name="email"
+        label="E-mail"
+        rules={[
+          {
+            type: "email",
+            message: "The input is not valid E-mail!",
+          },
+          {
+            required: true,
+            message: "Please input your E-mail!",
+          },
+        ]}
+      >
+        <Input
+          onChange={(event) =>
+            setUserInputData({
+              ...userInputData,
+              email: event.target.value,
+            })
+          }
+        />
+      </Form.Item>
+      <Form.Item
+        name="password"
+        label="Password"
+        rules={[
+          {
+            required: true,
+            message: "Please input your password!",
+          },
+          { min: 6, message: "Password should be more than 5 character" },
+          {
+            pattern: /[A-Z]/,
+            message: "Password must contain an uppercase",
+          },
+          { pattern: /[0-9]/, message: "Password must contain a number" },
+        ]}
+        hasFeedback
+      >
+        <Input.Password
+          onChange={(event) =>
+            setUserInputData({
+              ...userInputData,
+              password: event.target.value,
+            })
+          }
+        />
+      </Form.Item>
 
-        <Form.Item
-          name="confirm"
-          label="Confirm Password"
-          dependencies={["password"]}
-          hasFeedback
-          rules={[
-            {
-              required: true,
-              message: "Please confirm your password!",
+      <Form.Item
+        name="confirm"
+        label="Confirm Password"
+        dependencies={["password"]}
+        hasFeedback
+        rules={[
+          {
+            required: true,
+            message: "Please confirm your password!",
+          },
+          ({ getFieldValue }) => ({
+            validator(_, value) {
+              if (!value || getFieldValue("password") === value) {
+                return Promise.resolve();
+              }
+              return Promise.reject(new Error("The Passwords doesn't match!"));
             },
-            ({ getFieldValue }) => ({
-              validator(_, value) {
-                if (!value || getFieldValue("password") === value) {
-                  return Promise.resolve();
-                }
-                return Promise.reject(
-                  new Error("The two passwords that you entered do not match!")
-                );
-              },
-            }),
-          ]}
+          }),
+        ]}
+      >
+        <Input.Password />
+      </Form.Item>
+      <Form.Item
+        name="agreement"
+        valuePropName="checked"
+        rules={[
+          {
+            validator: (_, value) =>
+              value
+                ? Promise.resolve()
+                : Promise.reject(new Error("Should accept agreement")),
+          },
+        ]}
+        {...tailFormItemLayout}
+      >
+        <Checkbox>
+          I have read the <a href="/agreement">agreement</a>
+        </Checkbox>
+      </Form.Item>
+      <Form.Item {...tailFormItemLayout}>
+        <Button
+          type="primary"
+          htmlType="submit"
+          loading={loading}
+          style={{ marginTop: "20px" }}
         >
-          <Input.Password />
-        </Form.Item>
-        <Form.Item
-          name="agreement"
-          valuePropName="checked"
-          rules={[
-            {
-              validator: (_, value) =>
-                value
-                  ? Promise.resolve()
-                  : Promise.reject(new Error("Should accept agreement")),
-            },
-          ]}
-          {...tailFormItemLayout}
-        >
-          <Checkbox>
-            I have read the <a href="/agreement">agreement</a>
-          </Checkbox>
-        </Form.Item>
-        <Form.Item {...tailFormItemLayout}>
-          <Button type="primary" htmlType="submit" loading={loading}>
-            {loading ? "" : "Register"}
-          </Button>
-          Or <Link to="/login">Login now!</Link>
-        </Form.Item>
-      </Form>
-    </React.Fragment>
+          {loading ? "" : "Register"}
+        </Button>
+        Or <Link to="/login">Login now!</Link>
+      </Form.Item>
+    </Form>
   );
 };
 
