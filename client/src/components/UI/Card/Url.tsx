c97c6f97d91e0bee6ede9e8a0c54d8794a4a9f3a
surajkarki66 <suraj.karki500@protonmail.com>
2021-04-24 14:08:21 +0545

    Finished copy in url card

--- client/src/components/UI/Card/Url.tsx
+++ client/src/components/UI/Card/Url.tsx
@@ -1,12 +1,76 @@
-import { Card } from "antd";
-import React from "react";
+import moment from "moment";
+import { Button, Card, Col, Row, notification } from "antd";
+import React, { useState } from "react";
+import { CopyToClipboard } from "react-copy-to-clipboard";
 
-const UrlCard: React.FC = () => {
+import { UrlType } from "../../../pages/Home";
+import { BarChartOutlined } from "@ant-design/icons";
+
+type PropsType = {
+  url: UrlType;
+  fullName: string;
+};
+const UrlCard: React.FC<PropsType> = (props) => {
+  const { fullName } = props;
+  const { createdAt, title, shortUrl, longUrl, accessedDates } = props.url;
+  const [isCopied, setIsCopied] = useState(false);
+
+  const onCopyText = () => {
+    setIsCopied(true);
+    setTimeout(() => {
+      setIsCopied(false);
+    }, 1000);
+  };
   return (
     <Card>
-      <p>Card content</p>
-      <p>Card content</p>
-      <p>Card content</p>
+      <Row>
+        <Col>
+          <h4 style={{ color: "#666666" }}>
+            CREATED &nbsp;&nbsp;{moment(createdAt).format("LLL")}&nbsp; | &nbsp;{" "}
+            <p style={{ display: "inline", color: "#5280e9" }}>{fullName}</p>{" "}
+          </h4>
+          <h2>{title ? title : longUrl}</h2>
+          <h4 style={{ color: "#666666" }}>{longUrl}</h4>
+          <br />
+          <a rel="noreferrer" href={shortUrl} target="_blank">
+            {shortUrl.slice(7)}
+          </a>
+        </Col>
+        <Col span={24}>
+          <h3 style={{ float: "right" }}>
+            {" "}
+            {accessedDates && accessedDates.length}&nbsp;&nbsp;&nbsp;
+            <BarChartOutlined />
+            {accessedDates && accessedDates.length > 1 ? (
+              <p style={{ fontSize: 12 }}>
+                {moment(accessedDates[accessedDates.length - 1]).fromNow()}
+              </p>
+            ) : (
+              <p style={{ fontSize: 12 }}>Not Accessed</p>
+            )}
+          </h3>
+          <CopyToClipboard text={shortUrl} onCopy={onCopyText}>
+            <Button
+              type="primary"
+              size="small"
+              style={{ marginRight: 20, fontSize: 10, marginTop: 20 }}
+            >
+              COPY
+            </Button>
+          </CopyToClipboard>
+          {isCopied && notification.success({ message: "Copied" })}
+          <Button size="small" style={{ marginRight: 20, fontSize: 10 }}>
+            EDIT
+          </Button>
+          <Button
+            type="link"
+            size="small"
+            style={{ marginRight: 20, fontSize: 10, color: "red" }}
+          >
+            DELETE
+          </Button>
+        </Col>
+      </Row>
     </Card>
   );
 };
--- client/src/containers/Url/Url.tsx
+++ client/src/containers/Url/Url.tsx
@@ -1,18 +1,26 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Row, Col } from "antd";
-import { LinkOutlined } from "@ant-design/icons";
 
 import UrlCard from "../../components/UI/Card/Url";
 import { UserType } from "../../pages/Home";
+import { AuthContext } from "../../context/AuthContext";
 
 type PropsType = { user: UserType };
 
 const Url: React.FC<PropsType> = (props) => {
+  const { fullName } = useContext(AuthContext);
   const { urls } = props.user;
   return (
-    <div style={{ overflowY: "auto" }}>
+    <div>
       <Row>
-        <Col span={24}>{urls && urls.map((url) => <UrlCard />)}</Col>
+        <Col span={6}></Col>
+        <Col span={12}>
+          {urls &&
+            urls.map((url) => (
+              <UrlCard key={url.code} url={url} fullName={fullName} />
+            ))}
+        </Col>
+        <Col span={6}></Col>
       </Row>
     </div>
   );
