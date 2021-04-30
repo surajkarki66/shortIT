f678504356c0fa48221791d92ae90d14389c23ce
surajkarki66 <suraj.karki500@protonmail.com>
2021-04-30 14:17:49 +0545

    Finished ChangePasswordForm component

---
+++ client/src/components/Forms/ChangePasswordForm.tsx
@@ -0,0 +1,99 @@
+import React from "react";
+import { Form, Input, Button, FormInstance } from "antd";
+
+type Props = {
+  form: FormInstance<any>;
+  loading: boolean;
+  changeError: string;
+  onFinish: (value: any) => void;
+};
+
+const ChangePasswordForm: React.FC<Props> = (props) => {
+  const { loading, changeError, onFinish, form } = props;
+  return (
+    <Form
+      form={form}
+      style={{
+        minWidth: "405px",
+        fontWeight: "bold",
+      }}
+      name="change_password"
+      className="change-form"
+      initialValues={{ remember: true }}
+      onFinish={onFinish}
+    >
+      {changeError !== "" && <h4 style={{ color: "red" }}>{changeError}</h4>}
+      <Form.Item
+        name="oldPassword"
+        rules={[
+          {
+            required: true,
+            message: "Please input your old password!",
+          },
+          { min: 6, message: "Old Password should be more than 5 character" },
+          {
+            pattern: /[A-Z]/,
+            message: "Old Password must contain an uppercase",
+          },
+          { pattern: /[0-9]/, message: "Old Password must contain a number" },
+        ]}
+        hasFeedback
+      >
+        <Input.Password name="oldPassword" placeholder="Enter Old Password" />
+      </Form.Item>
+      <Form.Item
+        name="newPassword"
+        rules={[
+          {
+            required: true,
+            message: "Please input your password!",
+          },
+          { min: 6, message: "New Password should be more than 5 character" },
+          {
+            pattern: /[A-Z]/,
+            message: "New Password must contain an uppercase",
+          },
+          { pattern: /[0-9]/, message: "New Password must contain a number" },
+        ]}
+        hasFeedback
+      >
+        <Input.Password name="newPassword" placeholder="Enter New Password" />
+      </Form.Item>
+
+      <Form.Item
+        name="confirm"
+        dependencies={["newPassword"]}
+        hasFeedback
+        rules={[
+          {
+            required: true,
+            message: "Please confirm your password!",
+          },
+          ({ getFieldValue }) => ({
+            validator(_, value) {
+              if (!value || getFieldValue("newPassword") === value) {
+                return Promise.resolve();
+              }
+              return Promise.reject(new Error("The Passwords doesn't match!"));
+            },
+          }),
+        ]}
+      >
+        <Input.Password placeholder="Enter Confirm Password" />
+      </Form.Item>
+
+      <Form.Item>
+        <Button
+          loading={loading}
+          type="primary"
+          htmlType="submit"
+          className="change-password-button"
+        >
+          {loading ? "" : "Submit"}
+        </Button>
+      </Form.Item>
+    </Form>
+  );
+};
+
+export default ChangePasswordForm;
--- client/src/pages/Account.tsx
+++ client/src/pages/Account.tsx
@@ -1,7 +1,80 @@
-import React from "react";
+import React, { useState, useContext, useEffect } from "react";
+import { Collapse, Form, notification } from "antd";
+
+import Axios from "../axios-url";
+import ChangePasswordForm from "../components/Forms/ChangePasswordForm";
+import { AuthContext } from "../context/AuthContext";
+
+const { Panel } = Collapse;
 
 const Account: React.FC = () => {
-  return <h1>Account</h1>;
+  const [form] = Form.useForm();
+  const [loading, setLoading] = useState(false);
+  const [changeEmailError, setChangeEmailError] = useState("");
+  const [changePassError, setChangePassError] = useState("");
+  const [successPassChange, setSuccessPassChange] = useState(false);
+  const { userId, token } = useContext(AuthContext);
+
+  useEffect(() => {
+    if (successPassChange) {
+      notification.info({ message: "Password is changed successfully" });
+    }
+  }, [successPassChange]);
+
+  const onFinishPassChange = (values: any) => {
+    if (values) {
+      const { oldPassword, newPassword } = values;
+      changePassword(oldPassword, newPassword, userId, token);
+    }
+  };
+
+  const changePassword = (
+    oldPassword: string,
+    newPassword: string,
+    userId: string,
+    token: string
+  ) => {
+    setLoading(true);
+    Axios.post(
+      `/api/users/changePassword/${userId}`,
+      {
+        oldPassword,
+        newPassword,
+      },
+      {
+        headers: { Authorization: `Bearer ${token}` },
+      }
+    )
+      .then((res) => {
+        setLoading(false);
+        setChangePassError("");
+        setSuccessPassChange(true);
+        form.resetFields();
+      })
+      .catch((error) => {
+        const { data } = error.response;
+        setLoading(false);
+        setSuccessPassChange(false);
+        setChangePassError(data.data.error);
+      });
+  };
+  return (
+    <Collapse>
+      <Panel header="Change Password" key="1">
+        <div className="changePassword">
+          <ChangePasswordForm
+            form={form}
+            loading={loading}
+            changeError={changePassError}
+            onFinish={onFinishPassChange}
+          />
+        </div>
+      </Panel>
+      <Panel header="Change Email" key="2">
+        <h1>Change Email</h1>
+      </Panel>
+    </Collapse>
+  );
 };
 
 export default Account;
