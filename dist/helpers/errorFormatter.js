59cb2c53567fbcc7f2fb1fbbc77d7178bf02df88
Suraj Karki <suraj.karki500@protonmail.com>
2021-05-02 19:49:21 +0545

    Delete dist directory

--- dist/helpers/errorFormatter.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function errorFormatter({ msg }) {
-    return `${msg}`;
-}
-exports.default = errorFormatter;
-//# sourceMappingURL=errorFormatter.js.map
