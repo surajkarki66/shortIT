2eca1a94aba76dd7f0f0664eccac8806c57ff850
surajkarki66 <suraj.karki500@protonmail.com>
2021-06-25 16:58:44 +0545

    Deleted dist folder

--- dist/helpers/response.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function writeServerResponse(res, responseData) {
-    const { result, statusCode, contentType } = responseData;
-    res.setHeader("Content-Type", contentType);
-    return res.status(statusCode).json(result);
-}
-exports.default = writeServerResponse;
-//# sourceMappingURL=response.js.map
