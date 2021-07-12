a3e22beb8ba4ba5a6b9a4a5c042c489ddebde393
surajkarki66 <suraj.karki500@protonmail.com>
2021-07-12 21:25:45 +0545

    Fixes some bugs

--- src/controllers/common.controller.ts
+++ src/controllers/common.controller.ts
@@ -1,11 +1,8 @@
-import path from "path";
 import { Request, Response, NextFunction, RequestHandler } from "express";
-import { validationResult } from "express-validator";
 
 import Url from "../models/Url";
 import GuestUrl from "../models/Guest";
 import ApiError from "../errors/apiError";
-import errorFormatter from "../helpers/errorFormatter";
 
 const goToUrl: RequestHandler = async (
   req: Request,
@@ -13,18 +10,6 @@ const goToUrl: RequestHandler = async (
   next: NextFunction
 ) => {
   try {
-    const errors = validationResult(req).formatWith(errorFormatter);
-    const options = {
-      root: path.join(__dirname, "../../public"),
-      dotfiles: "deny",
-      headers: {
-        "x-timestamp": Date.now(),
-        "x-sent": true,
-      },
-    };
-    if (!errors.isEmpty()) {
-      return res.sendFile("invalid.html", options);
-    }
     const { code } = req.params;
     const url = Url.findByCode(code);
     const guestUrl = GuestUrl.findByCode(code);
@@ -39,7 +24,7 @@ const goToUrl: RequestHandler = async (
       const { longUrl } = result[1];
       return res.redirect(longUrl);
     } else {
-      return res.sendFile("invalid.html", options);
+      return res.status(404);
     }
   } catch (error) {
     next(ApiError.internal(`Something went wrong: ${error.message}`));
