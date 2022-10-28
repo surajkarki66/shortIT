73e7fc2f568448a42dc53a8888288b5ebf6a5a8d
“Suraj <suraj.karki500@protonmail.com>
2022-10-28 19:59:41 +0545

    Modified for deployment

--- client/src/axios-url.ts
+++ client/src/axios-url.ts
@@ -4,7 +4,7 @@ const nodeEnv = "production";
 const instance = axios.create({
   baseURL:
     nodeEnv === "production"
-      ? "https://shortit6.herokuapp.com"
+      ? "https://web-production-1f15.up.railway.app"
       : "http://localhost:5000",
   withCredentials: true,
 });
